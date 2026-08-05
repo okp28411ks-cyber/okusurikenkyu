13d4525ee605d2d20bec4217721c181ac3bded6d
okp28411ks-cyber <okp.28411ks@gmail.com>
2026-08-06 00:43:40 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("お薬手帳アプリ起動");
+const SUPABASE_URL = "https://nmstudwvvmbttfhanuyu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_DagUSgICo4JTVxwimcZKDw_NYBHPzSf";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Supabase接続準備OK");
